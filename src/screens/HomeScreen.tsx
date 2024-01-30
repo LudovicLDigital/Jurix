cb9c93dcb477f36193217d9adbc0c5f41f4995ee
LudovicLDigital <ludovic.laurichesse@l-digital.net>
2024-01-30 19:30:20 +0100

    :ok_hand:<fix> now build on iOs

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {ScrollView, StyleSheet, Text} from 'react-native';
 import BackgroundContainer from '../component/backgroundContainer.tsx';
 import {useTranslation} from 'react-i18next';
 
@@ -7,9 +7,9 @@ const HomeScreen = () => {
   const {t} = useTranslation('homeScreen');
   return (
     <BackgroundContainer>
-      <View style={styles.container}>
+      <ScrollView contentContainerStyle={styles.container}>
         <Text style={styles.pitch}>{t('pitch')}</Text>
-      </View>
+      </ScrollView>
     </BackgroundContainer>
   );
 };
@@ -23,7 +23,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     marginHorizontal: 20,
-    marginVertical: '30%',
+    marginTop: '20%',
+    marginBottom: '10%',
     borderRadius: 20,
     paddingVertical: 5,
     paddingHorizontal: 10,
